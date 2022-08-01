fd3c0c2e835157017e877acef5732256941e2bfe
Kous <kous.takken@gmail.com>
2022-08-02 01:04:28 +0500

    build header on slickslider

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 $(function(){
     $('.header__slider').slick({
-        infinite: true
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="arrow-right">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="arrow-right">'
     })
 })
