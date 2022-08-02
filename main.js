0430b7687b8a44c8a0994326439b12454e00196c
Kous <kous.takken@gmail.com>
2022-08-02 05:22:18 +0500

    incoming

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ $(function(){
         infinite: true,
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="arrow-right">',
-        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="arrow-right">'
-    })
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="arrow-right">',
+        asNavFor: '.slider-dots',
+    });
+
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header-slider',
+    });
 })
